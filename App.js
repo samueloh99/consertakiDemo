32eeadad6ac60dd847ee4d57a173eec8235687a9
samuel oh <samuel_oh@hotmail.com>
2019-03-24 23:16:47 -0300

    second commit test

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from "./components/login/login"
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Login> </Login>
       </View>
     );
   }
@@ -13,7 +13,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ components/login/login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View , StyleSheet} from 'react-native';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Consertaki!</Text>
+        <Text>
+            Smartphones, Tablets,
+            Notebooks e Desktops
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    texts:{
+
+    }
+})
